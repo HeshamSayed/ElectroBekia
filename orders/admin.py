78a6927523cffb08ad3310a588b66891bb505870
MotazAbuElnasr <motazibrahim@outlook.com>
2019-05-21 05:07:09 +0200

    Add stats and styling admin index

--- orders/admin.py
+++ orders/admin.py
@@ -32,33 +32,35 @@ class OrderItemAdmin(admin.ModelAdmin):
             'all': ('css/admin/style.css',)
         }
 
-def new_orders(request):
-  new_orders = Order.objects.annotate(count=Count('items')).filter(order_status = 1)
-  orders_exists = True if new_orders else False
-  paginator = Paginator(new_orders, 9) # Show 9 orders per page
+def get_context(request, orders):
+  orders_exists = True if orders else False
+  paginator = Paginator(orders, 9) # Show 9 orders per page
   page = request.GET.get('page')
-  orders = paginator.get_page(page)
-  orders_info = [(order,order.count) for order in orders]
+  orders_paged = paginator.get_page(page)
+  orders_info = [(order,order.count) for order in orders_paged]
+  new_orders = Order.objects.filter(order_status = 1).count()
   context= {
     "exists" : orders_exists,
     "orders" : orders_info,
-    "page_ranges": reversed(range(1 , orders.paginator.num_pages + 1)),
+    "page_ranges": reversed(range(1 , orders_paged.paginator.num_pages + 1)),
+    "new_orders":new_orders
   }
+  return context
+def new_orders(request,extra_context):
+  orders = Order.objects.annotate(count=Count('items')).filter(order_status = 1)
+  context = get_context(request, orders)
   return render(request, 'orders/admin/orders.html',context)
 admin.site.register_view('new-orders', view=new_orders)
 
-def old_orders(request):
-  old_orders = Order.objects.annotate(count=Count('items')).filter(~Q(order_status = 1))
-  orders_exists = True if old_orders else False
-  paginator = Paginator(old_orders, 9) # Show 9 orders per page
-  page = request.GET.get('page')
-  orders = paginator.get_page(page)
-  orders_info = [(order,order.count) for order in orders]
-  context= {
-    "exists" : orders_exists,
-    "orders" : orders_info,
-    "page_ranges":reversed(range(1 , orders.paginator.num_pages + 1)),
-  }
+def waiting_orders(request,extra_context):
+  orders = Order.objects.annotate(count=Count('items')).filter(order_status = 2)
+  context = get_context(request, orders)
+  return render(request, 'orders/admin/orders.html',context)
+admin.site.register_view('waiting-orders', view=waiting_orders)
+
+def old_orders(request,extra_context):
+  orders = Order.objects.annotate(count=Count('items')).filter(order_status = 3)
+  context = get_context(request, orders)
   return render(request, 'orders/admin/orders.html',context)
 admin.site.register_view('old-orders', view=old_orders)
 
